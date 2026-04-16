74012594a16d6ce7a789e8e2320cc9f4f113a04a
Doston <amonovdoston03@gmail.com>
2026-04-17 01:24:10 +0500

    1

--- main/urls.py
+++ main/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:test_id>/ready_to_test/', views.ready_to_test, name='ready_to_test'),
-    path('<int:test_id>/test/', views.test, name='test'),
+    path('test/<int:test_id>/', views.ready_to_test, name='ready_to_test'),
+    path('test/<int:test_id>/start/', views.test, name='test'),
     path('result/<int:checktest_id>/', views.test_result, name='test_result'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('create-test/', views.create_test, name='create_test'), 
-    path('add-question-field/', views.add_question_field, name='add_question_field'), # <- QO'SHING
-    path('my-tests/', views.my_tests, name='my_tests'),  # <- QO'SHING
-    path('test/<int:test_id>/detail/', views.test_detail, name='test_detail'),  # <- QO'SHING
+    path('add-question-field/', views.add_question_field, name='add_question_field'),
+    path('my-tests/', views.my_tests, name='my_tests'),
+    path('test/<int:test_id>/manage/', views.test_detail, name='test_detail'),
     path('test/<int:test_id>/import-questions/', views.import_questions, name='import_questions'),
     path('test/<int:test_id>/parse-questions/', views.parse_questions, name='parse_questions'),
     path('test/<int:test_id>/mass-create/', views.mass_create_questions, name='mass_create_questions'),
